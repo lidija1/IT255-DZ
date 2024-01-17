124bc34891ba3df0fb5d087ba0d0c41d69c28f5e
Lidija <lidija.dubovac01@gmail.com>
2024-01-17 14:18:00 +0100

    IT225-DZ11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,15 @@ import { PreporukeComponent } from './preporuke/preporuke.component';
 import { OnamaComponent } from './onama/onama.component';
 import { PhonesComponent } from './phones/phones.component';
 import { ReactiveFormsModule } from '@angular/forms'; //za validaciju
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
   { path: 'ponuda', component: PonudeComponent },
   { path: 'preporuka', component: PreporukeComponent },
+  { path: 'navbar', component: NavbarComponent },
   { path: 'onama', component: OnamaComponent },
   { path: 'phones', component: PhonesComponent },
+  { path: '', redirectTo: '/phones', pathMatch:'full' },
 ];
 
 @NgModule({
